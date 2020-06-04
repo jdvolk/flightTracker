c3d590ac74a92aa8730047fd227bc9b0952a8ad0
jdvolk <justin.d.volk@gmail.com>
2020-06-04 13:07:15 -0600

    set up scss files and style login page/ adding form and input fields and button

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './css/index.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/dark-background.jpg'
+import './images/login-avatar.svg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
