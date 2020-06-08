13983ee1b9fb5cd915a5e94dfdf8acd1a3db0300
jdvolk <justin.d.volk@gmail.com>
2020-06-07 20:38:03 -0600

    add login function/ event listener/ imported modules

--- src/index.js
+++ src/index.js
@@ -10,4 +10,36 @@ import './images/turing-logo.png'
 import './images/dark-background.jpg'
 import './images/login-avatar.svg'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import DataRepo from './DataRepo'
+import DomUpdates from './DomUpdates'
+import validateLogin from './validateLogin'
+import {Traveler, UserTypeTraveler} from './Traveler'
+import {Agency, UserTypeAgency} from './Agency'
+
+const loginButton = document.querySelector('.login');
+let form = document.querySelector('.login-form')
+let userName = document.querySelector('.user-name');
+let pass = document.querySelector('.pass');
+
+// let login, traveler,agency, dataRepo, domUpdates;
+
+
+loginButton.addEventListener('click', login)
+
+async function login(event) {
+  event.preventDefault();
+  let user = await validateLogin(userName.value, pass.value);
+  // trigger class of hidden on login page 
+  if(user.type === UserTypeTraveler) {
+    // trigger hope page if user is traveler
+    DomUpdates.toggleTraveler()
+    DomUpdates.hideLogin()
+  } else if (user.type === UserTypeAgency) {
+    // trigger home page if user is agency
+    DomUpdates.toggleAgency();
+    DomUpdates.hideLogin()
+  } else {
+    // error for failed login
+    console.log('error')
+  }
+}
