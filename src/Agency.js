8bea64c5bd32a697bdf9af25b541cd8ee5c793ed
jdvolk <justin.d.volk@gmail.com>
2020-06-08 17:58:10 -0600

    finish methods for agency class

--- src/Agency.js
+++ src/Agency.js
@@ -1,4 +1,5 @@
 const UserTypeAgency = "agency"
+import moment from 'moment'; moment().format(); 
 class Agency {
   constructor() {
     this.type = UserTypeAgency
@@ -10,16 +11,27 @@ class Agency {
     }
     return null
   }
-  totalIncomeThisYear(trips, destinations, date) {
-    // get total income generated this year 
-    // 10% of user trip cost 
-    // go through the trip data 
-    // for each item in the trip data 
-      // use the destination id to filter out destination
-      // add total cost per person from destination data
-      // multiply that by travelers from the trip data
-      // add that to the acc
-    // return acc * .1   
+  filterThisYearsTrips(trips) {
+    let startOfYear = moment().startOf('year').format('YYYY-MM-DD')
+    let tripsFromStartOfYear = trips.filter(trip => {
+      let tripDate = moment(trip.date, 'YYYY-MM-DD')
+      return moment(tripDate).isAfter(startOfYear)
+    })
+    return tripsFromStartOfYear
+  }
+  returnCostOfTrip(trip, destinations){
+    let currentDestination = destinations.find(destination => destination.id === trip.destinationID);
+    if(currentDestination === undefined) return 0
+    let costOfLodge = currentDestination.estimatedLodgingCostPerDay * trip.travelers 
+    let costOfFlight =  currentDestination.estimatedFlightCostPerPerson * trip.travelers
+    return costOfFlight + costOfLodge;
+  }
+  totalIncomeThisYear(trips, destinations) {
+    let tripsFromStartOfYear = this.filterThisYearsTrips(trips);
+    let result = tripsFromStartOfYear.reduce((totalSpent, trip) => {
+      return totalSpent += this.returnCostOfTrip(trip, destinations)  
+    }, 0)
+    return result * .1
   }
   filterTripsOnDate(trips, date) {
     if(typeof trips === 'object') {
@@ -28,10 +40,20 @@ class Agency {
     }
     return null
   }
-  travelersOnTripsToday(trips, date) {
+  activeTrips(trips) {
+    let today = moment()
+    let activeTrips = trips.filter(trip => {
+      let start = trip.date
+      let startFormatted = moment(start, 'YYYY-MM-DD')
+      let end = moment(start, 'YYYY-MM-DD').add(trip.duration,'days')
+      return moment(today).isBetween(startFormatted, end)
+    })
+    return activeTrips
+  }
+  travelersOnTripsToday(trips) {    
     if(typeof trips === 'object'){
-      let filteredResult = this.filterTripsOnDate(trips, date);
-      let result = filteredResult.map(person => person.userID)
+      let activeTrips = this.activeTrips(trips)
+      let result = activeTrips.map(person => person.userID)
       return result.length > 0 ? result : 'There are no travelers on trips today'
     }
     return null
--- test/Agency-test.js
+++ test/Agency-test.js
@@ -1,13 +1,13 @@
 import chai from 'chai';
 const expect = chai.expect;
 import {Agency, UserTypeAgency} from '../src/Agency'
-let agency, tripData, execution ;
+let agency, tripData, execution, newTripData, destinationData ;
 
 describe('Agency', () => {
   beforeEach(() => {
     agency = new Agency()
     tripData = [
-      {
+        {
           "id": 1,
           "userID": 44,
           "destinationID": 49,
@@ -16,8 +16,8 @@ describe('Agency', () => {
           "duration": 8,
           "status": "approved",
           "suggestedActivities": []
-      },
-      {
+        },
+        {
           "id": 2,
           "userID": 35,
           "destinationID": 25,
@@ -26,8 +26,8 @@ describe('Agency', () => {
           "duration": 18,
           "status": "pending",
           "suggestedActivities": []
-      },
-      {
+        },
+        {
           "id": 3,
           "userID": 3,
           "destinationID": 22,
@@ -36,8 +36,8 @@ describe('Agency', () => {
           "duration": 17,
           "status": "pending",
           "suggestedActivities": []
-      },
-      {
+        },
+        {
           "id": 4,
           "userID": 43,
           "destinationID": 14,
@@ -46,8 +46,8 @@ describe('Agency', () => {
           "duration": 10,
           "status": "approved",
           "suggestedActivities": []
-      },
-      {
+        },
+        {
           "id": 5,
           "userID": 42,
           "destinationID": 29,
@@ -56,8 +56,8 @@ describe('Agency', () => {
           "duration": 18,
           "status": "approved",
           "suggestedActivities": []
-      },
-      {
+        },
+        {
           "id": 6,
           "userID": 29,
           "destinationID": 35,
@@ -66,17 +66,56 @@ describe('Agency', () => {
           "duration": 9,
           "status": "approved",
           "suggestedActivities": []
-      },
+        },
+        {
+          id: 1591562556483,
+          userID: 50, 
+          destinationID: 3, 
+          travelers: 1, 
+          date: "2020/06/07",
+          "duration": 9,
+          "status": "approved",
+          "suggestedActivities": []
+        }
+      ]
+      destinationData = [
       {
-        id: 1591562556483,
-        userID: 50, 
-        destinationID: 3, 
-        travelers: 1, 
-        date: "2020/06/07",
-        "duration": 9,
-        "status": "approved",
-        "suggestedActivities": []
-      }
+          "id": 25,
+          "destination": "New York, New York",
+          "estimatedLodgingCostPerDay": 175,
+          "estimatedFlightCostPerPerson": 200,
+          "image": "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+          "alt": "people crossing the street during the day surrounded by tall buildings and advertisements"
+      },{
+          "id": 22,
+          "destination": "Rome, Italy",
+          "estimatedLodgingCostPerDay": 90,
+          "estimatedFlightCostPerPerson": 650,
+          "image": "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+          "alt": "people standing inside a colosseum during the day"
+      },{
+          "id": 14,
+          "destination": "Marrakesh, Morocco",
+          "estimatedLodgingCostPerDay": 70,
+          "estimatedFlightCostPerPerson": 830,
+          "image": "https://images.unsplash.com/photo-1517821362941-f7f753200fef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",
+          "alt": "people buying oranges and other fruit from a street vendor"
+      },{
+          "id": 29,
+          "destination": "Willemstad, Curaçao",
+          "estimatedLodgingCostPerDay": 80,
+          "estimatedFlightCostPerPerson": 1100,
+          "image": "https://images.unsplash.com/photo-1541748603027-cbfefa3a6c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80",
+          "alt": "brightly colored buildings near body of water"
+      },{
+        "id": 35,
+        "destination": "Anchorage, Alaska",
+        "estimatedLodgingCostPerDay": 200,
+        "estimatedFlightCostPerPerson": 100,
+        "image": "https://images.unsplash.com/photo-1539545547102-90ae2c140089?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "man riding on kayak surrounded by mountains"
+      },
+    
     ]
   })
   it('should be a function', () => {
@@ -149,10 +188,84 @@ describe('Agency', () => {
     })
   })
   describe('total income this year', () => {
-    it.skip('', () => {
-      
+    beforeEach(() => {
+      newTripData = [
+        {
+            "id": 1,
+            "userID": 44,
+            "destinationID": 49,
+            "travelers": 1,
+            "date": "2019/09/16",
+            "duration": 8,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+            "id": 2,
+            "userID": 35,
+            "destinationID": 25,
+            "travelers": 5,
+            "date": "2020/06/04",
+            "duration": 18,
+            "status": "pending",
+            "suggestedActivities": []
+        },
+        {
+            "id": 3,
+            "userID": 3,
+            "destinationID": 22,
+            "travelers": 4,
+            "date": "2020/06/05",
+            "duration": 17,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+            "id": 4,
+            "userID": 43,
+            "destinationID": 14,
+            "travelers": 2,
+            "date": "2020/06/25",
+            "duration": 10,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+            "id": 5,
+            "userID": 42,
+            "destinationID": 29,
+            "travelers": 3,
+            "date": "2020/04/30",
+            "duration": 18,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+            "id": 6,
+            "userID": 29,
+            "destinationID": 35,
+            "travelers": 3,
+            "date": "2020/06/06",
+            "duration": 9,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+          id: 1591562556483,
+          userID: 50, 
+          destinationID: 3, 
+          travelers: 1, 
+          date: "2020/06/07",
+          "duration": 9,
+          "status": "approved",
+          "suggestedActivities": []
+        }
+      ]
+
     })
-    it.skip('', () => {
+    it('should generate total income generated this year', () => {
+      let expected = agency.totalIncomeThisYear(newTripData, destinationData)
+      expect(expected).to.equal(1107.5)
 
     })
     it.skip('', () => {
@@ -160,6 +273,145 @@ describe('Agency', () => {
     })
     it.skip('', () => {
 
+    })
+    it.skip('', () => {
+
+    })
+  })
+  describe('active trips', () => {
+    beforeEach(() => {
+      newTripData = [
+        {
+            "id": 1,
+            "userID": 44,
+            "destinationID": 49,
+            "travelers": 1,
+            "date": "2019/09/16",
+            "duration": 8,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+            "id": 2,
+            "userID": 35,
+            "destinationID": 25,
+            "travelers": 5,
+            "date": "2020/06/04",
+            "duration": 18,
+            "status": "pending",
+            "suggestedActivities": []
+        },
+        {
+            "id": 3,
+            "userID": 3,
+            "destinationID": 22,
+            "travelers": 4,
+            "date": "2020/06/05",
+            "duration": 17,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+            "id": 4,
+            "userID": 43,
+            "destinationID": 14,
+            "travelers": 2,
+            "date": "2020/06/25",
+            "duration": 10,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+            "id": 5,
+            "userID": 42,
+            "destinationID": 29,
+            "travelers": 3,
+            "date": "2020/04/30",
+            "duration": 18,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+            "id": 6,
+            "userID": 29,
+            "destinationID": 35,
+            "travelers": 3,
+            "date": "2020/06/06",
+            "duration": 9,
+            "status": "approved",
+            "suggestedActivities": []
+        },
+        {
+          id: 1591562556483,
+          userID: 50, 
+          destinationID: 3, 
+          travelers: 1, 
+          date: "2020/06/07",
+          "duration": 9,
+          "status": "approved",
+          "suggestedActivities": []
+        }
+      ]
+    })
+    it('should return 1 active trips', () => {
+      let expected = agency.activeTrips(tripData)
+      expect(expected).to.eql([
+        {
+          "date": "2020/06/07",
+          "destinationID": 3,
+          "duration": 9,
+          "id": 1591562556483,
+          "status": "approved",
+          "suggestedActivities": [],
+          "travelers": 1,
+          "userID": 50,
+       }
+      ])
+    })
+    it('should return some active trips', () => {
+      let expected = agency.activeTrips(newTripData)
+      expect(expected).to.eql([
+        {
+          "date": "2020/06/04",
+          "destinationID": 25,
+          "duration": 18,
+          "id": 2,
+          "status": "pending",
+          "suggestedActivities": [],
+          "travelers": 5,
+          "userID": 35,
+        },
+        {
+          "date": "2020/06/05",
+          "destinationID": 22,
+          "duration": 17,
+          "id": 3,
+          "status": "approved",
+          "suggestedActivities": [],
+          "travelers": 4,
+          "userID": 3,
+        },
+        {
+          "date": "2020/06/06",
+          "destinationID": 35,
+          "duration": 9,
+          "id": 6,
+          "status": "approved",
+          "suggestedActivities": [],
+          "travelers": 3,
+          "userID": 29,
+        },
+        {
+          "date": "2020/06/07",
+          "destinationID": 3,
+          "duration": 9,
+          "id": 1591562556483,
+          "status": "approved",
+          "suggestedActivities": [],
+          "travelers": 1,
+          "userID": 50,
+        }
+      ])
     })
   })
   describe('filter trips on a date', () => {
@@ -191,8 +443,8 @@ describe('Agency', () => {
     beforeEach(() => {
     })
     it('should filter trip data for date and return array of travelers', () => {
-      let execution = agency.travelersOnTripsToday(tripData, '2020/06/07') 
-      expect(execution).to.eql([50])
+      let execution = agency.travelersOnTripsToday(newTripData) 
+      expect(execution).to.eql([35, 3, 29, 50])
     })
     it('should return null if not passed data', () => {
       let sadPath = agency.travelersOnTripsToday()
@@ -203,7 +455,17 @@ describe('Agency', () => {
       expect(sadPath).to.equal(null)
     })
     it('should return string if no travelers on trips today', () => {
-      let execution = agency.travelersOnTripsToday(tripData, '2020/0/07') 
+      let execution = agency.travelersOnTripsToday(
+        [{
+        "id": 2,
+        "userID": 35,
+        "destinationID": 25,
+        "travelers": 5,
+        "date": "2020/10/04",
+        "duration": 18,
+        "status": "pending",
+        "suggestedActivities": []
+    },]) 
       expect(execution).to.equal('There are no travelers on trips today')
     })
   })
