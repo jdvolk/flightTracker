a26e357f8c119e475a68ef4389edd0662a5209e1
jdvolk <justin.d.volk@gmail.com>
2020-06-07 20:32:12 -0600

    add methods to fetch the data from the api

---
+++ src/DataRepo.js
@@ -0,0 +1,25 @@
+export default class DataRepo {
+  constructor() {
+    this.travelers;
+    this.trips;
+    this.destinations
+  }
+
+  async getData(data) {
+    const url = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/'
+    
+    const response = await fetch(url + `${data}/${data}`)
+    const json = await response.json()
+    return json;
+  }
+
+  async getAllData(){
+    let travelers = await this.getData('travelers');
+    let trips = await this.getData('trips')
+    let destinations = await this.getData('destinations')
+    this.travelers = travelers;
+    this.trips = trips;
+    this.destinations = destinations;
+  }
+
+}
