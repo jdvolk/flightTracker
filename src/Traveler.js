9ac08ebb95620506d460dc48bc1653ebc83c24fa
jdvolk <justin.d.volk@gmail.com>
2020-06-08 20:09:43 -0600

    add tests to traveler class

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,12 +1,19 @@
 const UserTypeTraveler = "traveler"
+import moment from 'moment'; moment().format();
 class Traveler {
-  constructor() {
+  constructor(travelerData) {
+    this.id = travelerData.id
+    this.name = travelerData.name
+    this.travelerType = travelerData.travelerType
     this.type = UserTypeTraveler;
   }
-  filterTrips(){
+  filterPastTrips(trips){
     /*
     All of my trips (past, present, upcoming and pending)
     */
+  }
+  filterPresentTrips() {
+
   }
   returnTotalAmountSpent() {
     /*
@@ -14,6 +21,14 @@ class Traveler {
     This should be calculated from the trips data 
     and include a travel agent’s 10% fee
     */
+    // take in trip data
+    // date in destination data
+    // filter out all the trips using method above 
+    // reduce/for each item in the filtered data
+      // take the cost per person from the destination data, 
+      // multiply that by the amount of people on the trip
+      // add that to the acc
+      // return the acc * .10
 
   }
 
--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,21 +1,147 @@
 import chai from 'chai';
 const expect = chai.expect;
 import {Traveler, UserTypeTraveler} from '../src/Traveler'
-let traveler, tripData, execution, travelerData ;
+let traveler, tripData, execution, travelerData, destinationData;
 
 describe('traveler', () => {
   beforeEach(() => {
-    travelerData = 
+    tripData = [
+      {
+          "id": 1,
+          "userID": 44,
+          "destinationID": 49,
+          "travelers": 1,
+          "date": "2019/09/16",
+          "duration": 8,
+          "status": "approved",
+          "suggestedActivities": []
+      },
+      {
+          "id": 2,
+          "userID": 35,
+          "destinationID": 25,
+          "travelers": 5,
+          "date": "2020/06/04",
+          "duration": 18,
+          "status": "pending",
+          "suggestedActivities": []
+      },
+      {
+          "id": 3,
+          "userID": 3,
+          "destinationID": 22,
+          "travelers": 4,
+          "date": "2020/06/05",
+          "duration": 17,
+          "status": "approved",
+          "suggestedActivities": []
+      },
+      {
+          "id": 4,
+          "userID": 43,
+          "destinationID": 14,
+          "travelers": 2,
+          "date": "2020/06/25",
+          "duration": 10,
+          "status": "approved",
+          "suggestedActivities": []
+      },
+      {
+          "id": 5,
+          "userID": 42,
+          "destinationID": 29,
+          "travelers": 3,
+          "date": "2020/04/30",
+          "duration": 18,
+          "status": "approved",
+          "suggestedActivities": []
+      },
+      {
+          "id": 6,
+          "userID": 29,
+          "destinationID": 35,
+          "travelers": 3,
+          "date": "2020/06/06",
+          "duration": 9,
+          "status": "approved",
+          "suggestedActivities": []
+      },
+      {
+        id: 1591562556483,
+        userID: 50, 
+        destinationID: 3, 
+        travelers: 1, 
+        date: "2020/06/07",
+        "duration": 9,
+        "status": "approved",
+        "suggestedActivities": []
+      }
+    ];
+    destinationData = [
+      {
+          "id": 25,
+          "destination": "New York, New York",
+          "estimatedLodgingCostPerDay": 175,
+          "estimatedFlightCostPerPerson": 200,
+          "image": "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+          "alt": "people crossing the street during the day surrounded by tall buildings and advertisements"
+      },{
+          "id": 22,
+          "destination": "Rome, Italy",
+          "estimatedLodgingCostPerDay": 90,
+          "estimatedFlightCostPerPerson": 650,
+          "image": "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+          "alt": "people standing inside a colosseum during the day"
+      },{
+          "id": 14,
+          "destination": "Marrakesh, Morocco",
+          "estimatedLodgingCostPerDay": 70,
+          "estimatedFlightCostPerPerson": 830,
+          "image": "https://images.unsplash.com/photo-1517821362941-f7f753200fef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",
+          "alt": "people buying oranges and other fruit from a street vendor"
+      },{
+          "id": 29,
+          "destination": "Willemstad, Curaçao",
+          "estimatedLodgingCostPerDay": 80,
+          "estimatedFlightCostPerPerson": 1100,
+          "image": "https://images.unsplash.com/photo-1541748603027-cbfefa3a6c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80",
+          "alt": "brightly colored buildings near body of water"
+      },{
+        "id": 35,
+        "destination": "Anchorage, Alaska",
+        "estimatedLodgingCostPerDay": 200,
+        "estimatedFlightCostPerPerson": 100,
+        "image": "https://images.unsplash.com/photo-1539545547102-90ae2c140089?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "man riding on kayak surrounded by mountains"
+      }];
+      
+    travelerData = {
+      "id": 1,
+      "name": "Ham Leadbeater",
+      "travelerType": "relaxer"
+    },
 
-    traveler = new Traveler()
+    traveler = new Traveler(travelerData)
   })
   it('should be a function', () => {
     expect(Traveler).to.be.a('function')
   })
-  it('should be an instance of Agency', () => {
+  it('should be an instance of Traveler', () => {
     expect(traveler).to.be.an.instanceof(Traveler)
   })
   it('should have default property of agency', () => {
     expect(traveler.type).to.equal('traveler')
+  })
+  describe('filter past/present/future/pending trips', () => {
+    
+  })
+  describe('', () => {
+
+  })
+  describe('', () => {
+
+  })
+  describe('total amount spent', () => {
+
   })
 })
